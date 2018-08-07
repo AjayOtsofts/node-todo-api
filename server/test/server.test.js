7ab363175f34b891c2d778613ab93cf55c954c8d
SANJEEV <sanjeev@SANJEEVs-MacBook-Pro.local>
2018-08-07 14:55:14 +0530

    update server.test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
+const todos = [{
+  text: 'First Item'
+},{
+  text: 'Second Item'
+},{
+  text: 'Third Item'
+}];
+
 beforeEach((done) => {
-  Todo.remove({}).then(()  => done());
+  Todo.remove({}).then(()  => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST  /todos', () => {
@@ -19,18 +29,31 @@ describe('POST  /todos', () => {
       .send({text})
       .expect(200)
       .expect((res) => {
-        console.log(res.body);
+        //console.log(res.body);
         expect(res.body.text).toBe(text);
       })
       .end((err, res) => {
          if(err) {
            return done(err);
          }
-        Todo.find().then((todos) => {
+        Todo.find({text: text, completed: false}).then((todos) => {
           expect(todos.lenght).toBe(undefined);
           expect(todos[0].text).toBe(text);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+describe('GET /todos', () => {
+
+  it('Should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        //expect(res.body.todos.completed).toBe(2);
+      })
+      .end(done);
   });
 });
